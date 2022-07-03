a22ffa4028ddf2648cef13f8ab16cefe637d862a
Rayan <rayanchambet1@gmail.com>
2022-07-03 05:32:00 +0200

    V.0.9.1
    Main functions ✅
    Responsive Menu ✅
    Make it responsive 🎯

--- utils/menu.js
+++ utils/menu.js
@@ -1,4 +1,11 @@
-// navicon.addEventListener('click', () =>{
-//     navicon.classList.toggle('open');
-//     nav.classList.toggle('hidden');
-// } )
+// Displays menu and triggers burger animation
+navicon.addEventListener('click', () =>{
+    if(navicon.classList.contains('open') && !navMenu.classList.contains('hidden')){
+        navicon.classList.toggle('open');
+        navMenu.classList.add('hidden');
+    }
+    else{
+        navicon.classList.toggle('open');
+        navMenu.classList.remove('hidden');
+    }
+} )
